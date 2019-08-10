df2cab2fc66d0d554259e8bf047cc323ebe295f7
basxib <baselkhateeb@gmail.com>
2019-08-10 19:12:35 +0200

    Check array order

---
+++ CSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        public static string IsSortedAndHow(int[] array)
+        {
+            bool asc = true;
+            for(int i=1; i< array.Length ;i++)
+            {
+            if(array[i] > array[i-1] && asc)
+                continue;
+            else if(array[i] < array[i-1] && asc && i> 1)
+                return "no";
+            else if(array[i] < array[i-1])
+                asc = false;
+            }
+            if(asc) return "yes, ascending"; else return "yes, descending";
+        }
+        
+        static void Main(string[] args)
+        {
+            var s = IsSortedAndHow(new [] { 15, 7, 3, -8 });
+            Console.Write(s);
+        }
+    }
+}
