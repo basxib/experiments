4008487053b51567055508c0a72b397a78095099
basxib <baselkhateeb@gmail.com>
2019-08-25 19:06:41 +0200

    Add xUnit testing framework

--- CSharp/CardanoRover/Program.cs
+++ CSharp/CardanoRover/Program.cs
@@ -7,6 +7,10 @@ namespace CardanoRover
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Rover r = new Rover(new Plateau(5, 5), 1, 2, Orientation.North);
+            r.Rotate('R');
+            r.Move();
+            Console.WriteLine(r.PositionX  + " " + r.PositionY);
         }
     }
 }
