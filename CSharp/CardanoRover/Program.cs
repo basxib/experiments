7e23cf023db91050b963ba7b51e770fa29e66624
basxib <baselkhateeb@gmail.com>
2019-08-25 21:40:04 +0200

    Add CLI interface

--- CSharp/CardanoRover/Program.cs
+++ CSharp/CardanoRover/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using CommandLine;
+using System.Collections.Generic;
 
 namespace CardanoRover
 {
+    public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+        }
+
     class Program
     {
+        List<Rover> DeployedRovers;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string cmd = Console.ReadLine();
+            var plateauDimensions = cmd.Split(" ");
+            
+            while(cmd!= null)
+            {
+                var initialPosition = Console.ReadLine().Split(" ");
+                var movementSequence = Console.ReadLine();
+            }
+            
+
             Rover r = new Rover(new Plateau(5, 5), 1, 2, Orientation.North);
             r.Rotate('R');
             r.Move();
